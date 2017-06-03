92106684d5a237a73edc089d4d5a361cf67f2981
Daniel Qian <chanjarster@gmail.com>
2017-06-03 22:07:00 +0800

    polish concurrent collection happen before example

--- src/main/java/happen_before/ConcurrentCollectionHappenBeforeExample.java
+++ src/main/java/happen_before/ConcurrentCollectionHappenBeforeExample.java
@@ -1,12 +1,13 @@
 package happen_before;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
 /**
- * According to: <a href="https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/package-summary.html#package.description">javadoc</a> <br>
+ * 根据<a href="https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/package-summary.html#package.description">javadoc</a>的说法：<br>
  * <quote>Actions in a thread prior to placing an object into any concurrent collection happen-before actions subsequent to the access or removal of that element from the collection in another thread.</quote>
- * https://stackoverflow.com/a/32443705
  * Created by qianjia on 2017/6/3.
  */
 public class ConcurrentCollectionHappenBeforeExample {
@@ -18,12 +19,13 @@ public class ConcurrentCollectionHappenBeforeExample {
   public static void main(String[] args) throws InterruptedException {
 
     // neverEnds();
-    goodEnds();
+    // commonCollectionNeverEnds();
+    concurrentCollectionWillEnds();
   }
 
   /**
-   * 在这里例子里，t1有可能永远都不结束，因为stop变量的变化对其invisible。<br>
-   * 见<a href="https://stackoverflow.com/questions/461896/what-is-the-most-frequent-concurrency-issue-youve-encountered-in-java#comment348329_462648">SO的这个comment</a>得到一些灵感
+   * t1有可能永远都不结束，因为stop变量的变化对其invisible。<br>
+   * 见<a href="https://stackoverflow.com/questions/461896/what-is-the-most-frequent-concurrency-issue-youve-encountered-in-java#comment348329_462648">SO的这个comment</a>得到的灵感
    *
    * @throws InterruptedException
    */
@@ -48,9 +50,34 @@ public class ConcurrentCollectionHappenBeforeExample {
    *
    * @throws InterruptedException
    */
-  private static void goodEnds() throws InterruptedException {
+  private static void concurrentCollectionWillEnds() throws InterruptedException {
 
     ConcurrentLinkedQueue<Integer> conQueue = new ConcurrentLinkedQueue<>();
+
+    Thread t1 = new Thread(() -> {
+
+      while (!stop) {
+        conQueue.peek();
+      }
+      System.out.println(Thread.currentThread().getName() + " finished");
+    });
+
+    t1.start();
+
+    TimeUnit.MILLISECONDS.sleep(5000L);
+    stop = true;
+    conQueue.add(1);
+
+  }
+
+  /**
+   * 普通的 Collection 不具有 happen-before 特性，所以stop变量的变化对于t1 依然是 invisible
+   *
+   * @throws InterruptedException
+   */
+  private static void commonCollectionNeverEnds() throws InterruptedException {
+
+    Queue<Integer> conQueue = new LinkedList<>();
 
     Thread t1 = new Thread(() -> {
 
